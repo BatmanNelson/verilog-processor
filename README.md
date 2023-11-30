a49b8bab3dbd5bc9cc48efe04144b664d1c8d378
BatmanNelson <10joshn@gmail.com>
2023-11-30 12:40:06 -0700

    Getting verilog setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Verilog Processor
 Digital systems final project, a processor coded in Verilog for Basys 3 FPGA.
+
+Actual code is in ./verilog-processor.srcs/sources_1/new/
---
+++ verilog-processor.srcs/sources_1/new/top_module.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/30/2023 12:38:47 PM
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_module(
+    input [15:0] sw,
+    output [15:0] led
+    );
+endmodule
